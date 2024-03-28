2db71228e5fbb364e18870ab53aad5a342084566
Carson Strout <carson.strout42@gmail.com>
2024-03-27 18:38:36 -0600

    Created base SlotMachine functionality

---
+++ Six Shooter/Assets/Scripts/UpgradeSlotMachine.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeSlotMachine : MonoBehaviour
+{
+    [SerializeField] private GameObject[] slots;
+    private bool isSpinning = false;
+    [SerializeField] private float spinSpeed = 0.01f;
+    private int[] selectedUpgrades;
+    private bool beginSlowdown = false;
+    private float timer = 0f;
+
+    private void Start()
+    {
+        selectedUpgrades = new int[slots.Length];
+
+        for (int i = 0; i < slots.Length; i++)
+        {
+            selectedUpgrades[i] = Random.Range(0, slots[i].transform.childCount);
+            slots[i].transform.GetChild(selectedUpgrades[i]).gameObject.SetActive(true);
+        }
+
+        StartCoroutine(SpinSlots());
+    }
+
+    private void Update()
+    {
+        if (isSpinning && !beginSlowdown)
+        {
+            timer += Time.deltaTime;
+            Debug.Log(timer);
+
+            if (timer >= 3f)
+            {
+                beginSlowdown = true;
+                Debug.Log("Begin slowdown");
+            }
+        }
+    }
+
+    private IEnumerator SpinSlots()
+    {
+        isSpinning = true;
+        while (isSpinning)
+        {
+            for (int i = 0; i < slots.Length; i++)
+            {
+                slots[i].transform.GetChild(selectedUpgrades[i]).gameObject.SetActive(false);
+                selectedUpgrades[i] = Random.Range(0, slots[i].transform.childCount);
+                slots[i].transform.GetChild(selectedUpgrades[i]).gameObject.SetActive(true);
+            }
+
+            yield return new WaitForSeconds(spinSpeed);
+
+            if (beginSlowdown)
+            {
+                spinSpeed += 0.01f;
+
+                if (spinSpeed >= 0.25f)
+                {
+                    isSpinning = false;
+                    RandomizeUniqueFinalSelections();
+                }
+            }
+        }
+
+        EnableSelection();
+    }
+
+    private void RandomizeUniqueFinalSelections()
+    {
+        HashSet<int> usedSelections = new HashSet<int>();
+        for (int i = 0; i < slots.Length; i++)
+        {
+            int selection;
+            do
+            {
+                selection = Random.Range(0, slots[i].transform.childCount);
+            } while (usedSelections.Contains(selection));
+
+            usedSelections.Add(selection);
+            selectedUpgrades[i] = selection;
+        }
+
+        for (int i = 0; i < slots.Length; i++)
+        {
+            foreach (Transform child in slots[i].transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+            slots[i].transform.GetChild(selectedUpgrades[i]).gameObject.SetActive(true);
+        }
+    }
+
+    private void EnableSelection()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            slots[i].transform.GetChild(selectedUpgrades[i]).gameObject.GetComponent<BoxCollider>().enabled = true;
+        }
+
+        Debug.Log("Selection enabled");
+    }
+}
